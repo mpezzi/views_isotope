1b03dc013c87a42a8295b095504689cf7fe13414
Mike Pezzi <mpezzi@hhmedia.com>
2011-02-14 16:32:01 -0500

    Working 6.x demo

--- views-isotope.tpl.php
+++ views-isotope.tpl.php
@@ -2,6 +2,18 @@
 // $Id:
 
 ?>
-<div class="views-isotope item-list">
-  hello
+
+<ul style="padding-top: 100px">
+  <li id="sort-weight">Sort by Weight</li>
+  <li id="sort-category">Sort by Category</li>
+</ul>
+
+<div class="views-isotope">
+  <<?php print $options['type']; ?> class="views-isotope-container" id="<?php print $isotope_id; ?>">
+    <?php foreach ( $rows as $id => $row ): ?>
+      <li class="<?php print $classes[$id]; ?>" <?php print drupal_attributes($attributes[$id]); ?>>
+        <div class="item"><?php print $row; ?></div>
+      </li>
+    <?php endforeach; ?>
+  <<?php print $options['type']; ?>>
 </div>
--- views_isotope.js
+++ views_isotope.js
@@ -3,6 +3,32 @@
  */
 Drupal.behaviors.views_isotope = function() {
   jQuery.each(Drupal.settings.views_isotope, function(selector, options){
-    $(selector + ':not(.views-isotope-processed)').addClass('views-isotope-processed').isotope(options);
+    var settings = $.extend(options, {
+      animationEngine: $.browser.opera ? 'jquery' : 'best-available',
+      getSortData: {
+        category: function(e) {
+          return e.attr('data-category');
+        },
+        weight: function(e) {
+          return e.attr('data-weight');
+        }
+      }
+    });
+    
+    $('#sort-weight').click(function(){
+      $(selector).isotope({
+        sortBy: 'weight',
+        sortAscending: true
+      });
+    });
+    
+    $('#sort-category').click(function(){
+      $(selector).isotope({
+        sortBy: 'category',
+        sortAscending: true
+      });
+    });
+    
+    $(selector + ':not(.views-isotope-processed)').addClass('views-isotope-processed').isotope(settings);
   });
 };
