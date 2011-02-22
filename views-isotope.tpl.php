fecab649383bc82ebb32cbb7dbbf4d2e7c8d991a
Mike Pezzi <mpezzi@hhmedia.com>
2011-02-22 14:45:51 -0500

    Moved sorting inside views-isotope div

--- views-isotope.tpl.php
+++ views-isotope.tpl.php
@@ -1,20 +1,13 @@
-<?php
-// $Id:
-
-
-
-?>
-
-<?php if ( count($sortable_fields) ): ?>
-  <ul class="views-isotope-sort" style="padding-top: 100px">
-  <?php foreach ( $sortable_fields as $field => $label ): ?>
-    <li id="sort-<?php print $field; ?>" data-field="<?php print $field; ?>"><?php print t('Sort by %label', array('%label' => $label)); ?></li>
-  <?php endforeach; ?>
-  </ul>
-<?php endif; ?>
-
-<div class="views-isotope <?php print $skin_class; ?>">
-  <<?php print $options['type']; ?> class="views-isotope-container" id="<?php print $isotope_id; ?>">
+<div class="views-isotope <?php print $skin_class; ?>" id="<?php print $isotope_id; ?>">
+  <?php if ( count($sortable_fields) ): ?>
+    <ul class="views-isotope-sort">
+    <?php foreach ( $sortable_fields as $field => $label ): ?>
+      <li id="sort-<?php print $field; ?>" data-field="<?php print $field; ?>"><?php print t('Sort by %label', array('%label' => $label)); ?></li>
+    <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+  
+  <<?php print $options['type']; ?> class="views-isotope-container">
     <?php foreach ( $rows as $id => $row ): ?>
       <li class="<?php print $classes[$id]; ?>" <?php if ( isset($attributes[$id]) ) print drupal_attributes($attributes[$id]); ?>>
         <div class="item"><?php print $row; ?></div>
